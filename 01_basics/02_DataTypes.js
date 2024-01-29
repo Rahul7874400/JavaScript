030dda3c5aefab4f4225b6471b48ae5caa59f063
Rahul7874400 <rkkraj500@gmail.com>
2024-01-29 10:32:23 +0000

    DataTypes done

---
+++ 01_basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all vs code as new version javascript
+
+//alert(3+3) // we are using node not browser
+
+let name = "Rahul Kumar"
+let age = 24
+let isLoggedIn = false
+
+// Data types
+// nunmer
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol =>unique
+
+
+// object
+
+console.log(typeof "Rahul") // string
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
+
+
