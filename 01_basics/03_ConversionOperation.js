c8d5ca9c31bf65b12eee3fb1c88b0a41af5a0c88
Rahul7874400 <rkkraj500@gmail.com>
2024-01-29 10:54:14 +0000

    Operation conversion

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,16 @@
+let score = undefined
+
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "98" => 98
+// "98bsd" => NaN
+// true => 1; false =>0
+// 1 => true ; 0=> false
+// "" => false
+// "rahul" => true
+// 12 => string
