74d7f65fba0c9075ffa41ad6e54963278f1bb9ab
Rahul7874400 <rkkraj500@gmail.com>
2024-01-29 10:04:36 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 12324
+let accountName = "Rahul Kumar"
+var accountPassword = "1234"
+acountCity = "Patna"
+let accountState; // unintilize wil be undefine
+
+//accountId = 889879  //Not Allowed
+
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountName,accountPassword,acountCity,accountState]);
