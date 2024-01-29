b63189ed527ea396f3ef92c93f107ee25d3d6595
Rahul7874400 <rkkraj500@gmail.com>
2024-01-29 13:02:40 +0000

    Camparision done

---
+++ 01_basics/04_Camparision.js
@@ -0,0 +1,18 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2<=1)
+
+// console.log("2">1) // string converted into number
+// console.log("02">1)
+
+// console.log(null>1)
+// console.log(null>=1)
+// console.log(null==1)
+
+// console.log(undefined>1)
+// strict camparision (===)
+console.log("2" === 1)
+
+
+
