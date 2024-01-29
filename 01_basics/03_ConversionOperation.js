bdfcf9d64e223a2408123496e5b292f34c37abec
Rahul7874400 <rkkraj500@gmail.com>
2024-01-29 12:39:16 +0000

    Operator

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -14,3 +14,37 @@ console.log(valueInNumber);
 // "" => false
 // "rahul" => true
 // 12 => string
+
+
+
+
+//        ******************operator*********************
+
+let value = 3;
+let negValue = -value
+
+console.log(negValue); // => -3;
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**3)
+// console.log(2%2)
+
+let str1 = "Rahul"
+let str2 = " Kumar"
+
+let str3 = str1+str2;
+
+console.log(str3);
+
+console.log("1" + 2);// => 12
+console.log(1 + "2");//=>12
+
+console.log("1" + 2 + 2)// => 122
+
+console.log(1+2+"3")//=>33
+
+
+
