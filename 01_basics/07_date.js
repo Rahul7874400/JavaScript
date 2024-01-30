3a56580b31d3bbdc91e111b93635035eb485f4a9
Rahul7874400 <rkkraj500@gmail.com>
2024-01-30 06:33:40 +0000

    Date and time

---
+++ 01_basics/07_date.js
@@ -0,0 +1,21 @@
+// Date
+
+let myDate = Date();
+
+// console.log(myDate.toString())
+// console.log(myDate.toLocaleLowerCase())
+
+
+//let myCreateDate = Date(2024,0,30)
+
+let myCreateDate = Date("2024-01-30")
+//console.log(myCreateDate.toLocaleString())
+
+
+let myTimeStamp = Date.now();
+
+// console.log(myTimeStamp)
+// console.log(myCreateDate)
+
+let myDateName = new Date()
+console.log(myDateName.getMonth())
